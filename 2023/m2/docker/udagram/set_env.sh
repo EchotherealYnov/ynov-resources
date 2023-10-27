018e5d64e431e196aa109b5bdb9fcccf2fcbcf17
Echotherealynov <leo.magnant@ynov.com>
2023-10-27 15:34:05 +0000

    udagram

--- 2023/m2/docker/udagram/set_env.sh
+++ 2023/m2/docker/udagram/set_env.sh
@@ -2,10 +2,10 @@
 
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=<YOUR_POSTGRES_USER>
-export POSTGRES_PASSWORD=<YOUR_POSTGRES_PASSWORD>
-export POSTGRES_HOST=<YOUR_POSTGRES_HOST>
-export POSTGRES_DB=<YOUR_POSTGRES_DB_NAME>
+export POSTGRES_USERNAME=root
+export POSTGRES_PASSWORD=admin
+export POSTGRES_HOST=udagramdb
+export POSTGRES_DB=udagramdb
 export AWS_BUCKET=udagram-bucket-1234567 # Please dont change this
 export AWS_REGION=us-east-1 #Please dont change this
 export AWS_PROFILE=default #Please dont change this
